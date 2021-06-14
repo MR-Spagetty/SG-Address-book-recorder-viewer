f42bb4aea31abefb0d0b7eb7ad963efa5d97f697
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2021-06-14 20:17:39 +1200

    gave none glyph transparent background implemented editing for addresses added an example book

--- AddressBook.py
+++ AddressBook.py
@@ -18,55 +18,24 @@ class Address:
 
 def load_glyphs(path, folder, glyph_names):
     glyphs = {}
+    smol_glyphs = {}
     failed_to_load = []
 
     for glyph_name in glyph_names:
-        glyph_id = glyph_names.index(glyph_name)
-        # glyph_path = f'{path}Glyphs\{folder}\Glyph ({glyph_id}).png'
         glyph_path = f'{path}Glyphs\{folder}\{glyph_name}.png'
         if os.path.isfile(glyph_path):
             image = Image.open(glyph_path)
             image = image.resize((300, 300), Image.ANTIALIAS)
-            # gui.PhotoImage(file=glyph_path)
+            smol_image = image.resize((100, 100), Image.ANTIALIAS)
             glyphs[glyph_name] = ImageTk.PhotoImage(image)
+            smol_glyphs[glyph_name] = ImageTk.PhotoImage(smol_image)
         else:
             failed_to_load.append(glyph_name)
-    return glyphs, failed_to_load
+    return glyphs, smol_glyphs, failed_to_load
 
 
 def output_generator(book_to_output, books, out_path):
     book_items = []
-    for item in books[book_to_output]:
-        book_item = f"""
-name={book_to_output}
-mw
-glyph1={item['mw']['glyph1']}
-glyph2={item['mw']['glyph2']}
-glyph3={item['mw']['glyph3']}
-glyph4={item['mw']['glyph4']}
-glyph5={item['mw']['glyph5']}
-glyph6={item['mw']['glyph6']}
-glyph7={item['mw']['glyph7']}
-glyph8={item['mw']['glyph8']}
-peg
-glyph1={item['peg']['glyph1']}
-glyph2={item['peg']['glyph2']}
-glyph3={item['peg']['glyph3']}
-glyph4={item['peg']['glyph4']}
-glyph5={item['peg']['glyph5']}
-glyph6={item['peg']['glyph6']}
-glyph7={item['peg']['glyph7']}
-glyph8={item['peg']['glyph8']}
-uni
-glyph1={item['uni']['glyph1']}
-glyph2={item['uni']['glyph2']}
-glyph3={item['uni']['glyph3']}
-glyph4={item['uni']['glyph4']}
-glyph5={item['uni']['glyph5']}
-glyph6={item['uni']['glyph6']}
-glyph7={item['uni']['glyph7']}
-glyph8={item['uni']['glyph8']}"""
-        book_items.append(book_item)
 
 
 def setup():
@@ -93,7 +62,7 @@ def setup():
                        'Salma', 'Hamlinto', 'Elenami', 'Tahnan', 'Zeo',
                        'Roehi', 'Once El', 'Sandovi', 'Illume', 'Amiwill',
                        'Sibbron', 'Gilltin', 'Ramnon', 'Olavii', 'Hacemill',
-                       'Poco Re', 'Abrin', 'Earth']
+                       'Poco Re', 'Abrin', 'Baselai']
 
     UNI_GLYPHS_LIST = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9',
                        'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17',
@@ -101,27 +70,36 @@ def setup():
                        'g26', 'g27', 'g28', 'g29', 'g30', 'g31', 'g32', 'g33',
                        'g34', 'g35', 'g36']
 
+    global GLYPH_TYPES
     GLYPH_TYPES = {'MW': MW_GLYPHS_LIST,
                    'PEG': PEG_GLYPHS_LIST,
                    'UNI': UNI_GLYPHS_LIST}
 
     global loaded_glyphs
     loaded_glyphs = {}
+    global smol_loaded_glyphs
+    smol_loaded_glyphs = {}
 
     for folder, glyph_type in GLYPH_TYPES.items():
-        success, failed = load_glyphs(file_location, folder, glyph_type)
-        print(f"""{len(success)} glyphs were succesfully loaded
+        normal, smol, failed = load_glyphs(file_location, folder, glyph_type)
+        print(f"""{len(normal)} glyphs were succesfully loaded
 {len(failed)} glyphs failed to load
 glyphs that failed to load:\n{failed}""")
-        loaded_glyphs[folder] = success
+        loaded_glyphs[folder] = normal
+        smol_loaded_glyphs[folder] = smol
 
     global loaded_books
     loaded_books = {}
 
     global none_image
+    global smol_none_image
     image = Image.open(f'{file_location}Glyphs\\none.png')
     image = image.resize((300, 300), Image.ANTIALIAS)
     none_image = ImageTk.PhotoImage(image)
+    smol_image = image.resize((100, 100), Image.ANTIALIAS)
+    smol_none_image = ImageTk.PhotoImage(smol_image)
+
+    global current_displayed_glyphs
     current_displayed_glyphs = {'glyph1': 'none', 'glyph2': 'none',
                                 'glyph3': 'none', 'glyph4': 'none',
                                 'glyph5': 'none', 'glyph6': 'none',
@@ -129,35 +107,18 @@ glyphs that failed to load:\n{failed}""")
 
     def load_book():
         book_path = filedialog.askopenfilename(
-            filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
+            filetypes=(("JSON files", "*.json"), ("All files", "*.*")))
+        book_name = book_path.split('/')[-1]
         if os.path.isfile(book_path):
-            book_file = open(book_path, 'r')
-            book_content = book_file.readlines()
-            # Book file interperter
-            address_name = ''
-            glyph_type = ''
+            with open(book_path, 'r') as book_file:
+                book = json.load(book_file)
             book_items = {}
-
-            for line in book_content:
-                if 'name=' in line:
-                    address_name = line.split('=')[1].strip()
-                    book_items[address_name] = {}
-                elif 'mw' in line:
-                    glyph_type = 'mw'
-                    book_items[address_name][glyph_type] = {}
-                elif line == 'peg\n':
-                    glyph_type = 'peg'
-                    book_items[address_name][glyph_type] = {}
-                elif line == 'uni\n':
-                    glyph_type = 'uni'
-                    book_items[address_name][glyph_type] = {}
-                elif 'glyph' in line:
-                    glyph_num, glyph_name = line.strip().split('=')
-                    book_items[
-                        address_name][glyph_type][glyph_num] = glyph_name
-            book_path_order = book_path.split('/')
-            book_name = book_path_order[-1]
-            book_name_textvar.set(f'Book Name: {book_name}')
+            for name, entry in book.items():
+                book_items[name] = {}
+                for address_type, address in entry.items():
+                    book_items[name][address_type] = {}
+                    for item, value in address.items():
+                        book_items[name][address_type][item] = value
             return 'successfull', book_items, book_name
         else:
             book_name_textvar.set('Book Name: ')
@@ -199,14 +160,17 @@ glyphs that failed to load:\n{failed}""")
     selected_book.trace_add('write', address_menu_update)
 
     def change_displayed(x, y, z):
+        global current_displayed_glyphs
         selected_type = gui.StringVar.get(address_type_selected).lower()
         new_glyphs = []
         book = gui.StringVar.get(selected_book)
         adderss_name = gui.StringVar.get(selected_address)
-
-        new_glyphs = loaded_books[book][adderss_name][selected_type]
-        current_displayed_glyphs = new_glyphs
-        update_display(current_displayed_glyphs)
+        try:
+            new_glyphs = loaded_books[book][adderss_name][selected_type]
+            current_displayed_glyphs = new_glyphs
+            update_display(current_displayed_glyphs)
+        except KeyError:
+            pass
 
     selected_address = gui.StringVar()
     selected_address.trace_add('write', change_displayed)
@@ -231,19 +195,65 @@ glyphs that failed to load:\n{failed}""")
                          'glyph5': (1, 3), 'glyph6': (2, 3),
                          'glyph7': (0, 5), 'glyph8': (2, 5)}
 
+    edit_window = gui.Toplevel(display_window)
+    edit_window.withdraw()
+
+    def change_glyph_logic(id, glyph_name='none'):
+        global current_displayed_glyphs
+        current_displayed_glyphs[id] = glyph_name
+        update_display(current_displayed_glyphs)
+        finish_edit()
+
+    def change_glyph(id):
+        edit_window.title(f'editing {id}')
+        global GLYPH_TYPES
+        edit_window.deiconify()
+        global current_displayed_glyphs
+        print(current_displayed_glyphs[id])
+        test_button = gui.Button(edit_window, command=finish_edit)
+        test_button.grid()
+        buttons = {}
+        selected_type = gui.StringVar.get(address_type_selected)
+        for glyph in GLYPH_TYPES[selected_type]:
+            buttons[glyph] = gui.Button(edit_window,
+                                        image=smol_loaded_glyphs[
+                                            selected_type][glyph],
+                                        command=lambda glyph_name=glyph:
+                                            change_glyph_logic(id, glyph_name),
+                                            background='magenta2')
+        for glyph_name, button in buttons.items():
+            row, column = divmod(GLYPH_TYPES[selected_type].index(glyph_name), 6)
+            button.grid(row=row, column=column)
+        global smol_none_image
+        none_button = gui.Button(edit_window, image=smol_none_image,
+                                 background='magenta2', command=lambda: change_glyph_logic(id))
+        if column + 1 == 6:
+            row += 1
+            column = 0
+        else:
+            column += 1
+        none_button.grid(row=row, column=column)
+
+    def finish_edit():
+        for child in edit_window.winfo_children():
+            child.destroy()
+        edit_window.withdraw()
+
     global displays
     displays = {}
     global stringvars
     stringvars = {}
     for num in current_displayed_glyphs.keys():
         stringvars[num] = gui.StringVar(display_window, 'none')
-        displays[num] = (gui.Button(display_window),
+        displays[num] = (gui.Button(display_window, background="royal blue2",
+                                    command=lambda id=num: change_glyph(id)),
                          gui.Label(display_window,
                                    textvariable=stringvars[num]))
     for id, display in displays.items():
         colume, row = display_locations[id]
         display[0].grid(column=colume, row=row)
         display[1].grid(column=colume, row=row + 1)
+
     def update_display(glyphs):
         global stringvars
         global displays
@@ -252,10 +262,13 @@ glyphs that failed to load:\n{failed}""")
         for id, glyph in glyphs.items():
             try:
                 stringvars[id].set(glyph)
-                displays[id][0].configure(image=loaded_glyphs[gui.StringVar.get(address_type_selected)][glyph])
+                displays[id][0].configure(
+                    image=loaded_glyphs[gui.StringVar.get(
+                        address_type_selected)][glyph])
             except:
                 stringvars[id].set('none')
                 displays[id][0].configure(image=none_image)
+    update_display(current_displayed_glyphs)
 
 setup()
 root.mainloop()
